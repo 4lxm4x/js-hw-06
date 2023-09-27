f427f49016606e9d7a1a84471aede41f09a4314b
Oleksandr Maksymenko <machinery69@gmail.com>
2023-09-27 13:01:48 +0300

    finished task 8

--- js/task-08.js
+++ js/task-08.js
@@ -4,10 +4,31 @@ const refs = {
   password: document.querySelector("input[name='password']"),
   submitBtn: document.querySelector("button"),
 };
-console.log(refs);
-refs.submitBtn.addEventListener("submit", onSubmit);
+//console.log(refs.submitBtn);
+
+refs.form.addEventListener("submit", onSubmit);
 
 function onSubmit(event) {
-  console.log(event);
+  //window.alert;
   event.preventDefault();
+  const { email, password } = event.target.elements;
+  //console.log(event.target.elements.email.value);
+  if (email.value === "" || password.value === "") {
+    alert("Please fill out required fields");
+  } else {
+    const userData = {
+      email: email.value,
+      password: password.value,
+    };
+
+    const formData = new FormData(event.currentTarget);
+    formData.forEach((x, y) => {
+      console.log(x, y);
+    });
+
+    console.log(`Почта: ${userData.email} Пароль: ${userData.password} `);
+    refs.form.reset();
+  }
+
+  // console.log(email.value, password.value);
 }
