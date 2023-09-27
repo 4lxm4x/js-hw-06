10ce93cf6e7d941383d14f7898777aacc8ee615f
Oleksandr Maksymenko <machinery69@gmail.com>
2023-09-27 13:17:08 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,19 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  btn: document.querySelector(".change-color"),
+  currentColor: document.querySelector(".color"),
+  bodyEl: document.querySelector("body"),
+  styleEl: document.querySelector("style"),
+};
+
+refs.btn.addEventListener("click", onBtnClick);
+
+function onBtnClick() {
+  const chosedColor = getRandomHexColor();
+  //refs.styleEl.insertAdjacentElement('beforeend',`.body-styles{backg}`)
+  refs.bodyEl.style.backgroundColor = chosedColor;
+  refs.currentColor.textContent = chosedColor;
+}
